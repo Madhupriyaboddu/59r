34872f8004c325242800e711c8ef7981732340b6
Madhupriyaboddu <boddumadhupriya@gmail.com>
2025-07-21 11:01:18 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,28 @@
+
+# print(list(range(1,10)))
+# print(list(range(1,10,2)))
+# print(list(range(10,1,-1)))
+# print(list(range(10,1,-2)))
+# print(list(range(3,100,3)))
+# dict1 ={
+# 1:'madhu',
+# 2:'bunny',
+# 3:'madhavi',
+# 4:'rupa',
+# 5:'madhupriya',
+# 6:'keerthi',
+# '7':'madhu',
+# 3:'krishna'
+#    }
+
+# print(dict1)
+# print(dict1[5])
+# dict1['7'] ='alluarjun'
+
+# set1={1,2,3,4,5.5,'madhu',1,1,'madhu2'}
+# print(set1
+
+frzs1= frozenset([1,2,3,1,'str1','str2'])
+print(frzs1)
+
+
